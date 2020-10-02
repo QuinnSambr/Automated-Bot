dfb291821d4bc46a690b904e85940941d7b871c4
QuinnyBoy-TT <59735361+QuinnyBoy-TT@users.noreply.github.com>
2020-10-02 14:11:35 -0400

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ productURL={
 
 bbKey ={
         "product_url":"https://www.nvidia.com/en-us/shop/geforce/gpu/?page=1&limit=9&locale=en-us&category=GPU&sorting=lp&gpu=RTX%202060%20SUPER&gpu_filter=RTX%203090~1,RTX%203080~7,RTX%203070~1,TITAN%20RTX~1,RTX%202080%20Ti~2,RTX%202080%20SUPER~2,RTX%202070%20SUPER~7,RTX%202060%20SUPER~7,RTX%202060~16,GTX%201660%20Ti~7,GTX%201660%20SUPER~4,GTX%201660~11,GTX%201650%20SUPER~8,GTX%201650~12",
-        "FN":"Android",
-        "LN":"Poo Bear",
-        "STRADD":"Somewhere in cali",
-        "CT":"The ExactFucking City",
+        "FN":"John",
+        "LN":"Doe",
+        "STRADD":"2752 Oak Lane",
+        "CT":"Hopkins, MO 64461",
         "ST":"CA",
         "ZC":"00000",
-        "EMAIL":"myemail123@gmail.com",
-        "PN":"1627261726",
+        "EMAIL":"JeromeAMorrison@jourrapide.com",
+        "PN":"6607784779",
         "CC":'349057740654298'
 }
 
